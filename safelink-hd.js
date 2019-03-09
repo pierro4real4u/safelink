286f1c29d4f489cc09984f569ebbe2a288e51cde
pierro4real4u <48392712+pierro4real4u@users.noreply.github.com>
2019-03-09 20:57:12 +0000

    Update safelink-hd.js

--- safelink-hd.js
+++ safelink-hd.js
@@ -22,7 +22,5 @@ if(a_to_vh==false)
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
-{a_to_vk.innerHTML+=a_to_va;}}
+{A_to_vk.innerHTML + = a_to_va;}}
 function a_to_fa()
-{var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
-auto_safelink();
